08aa6db207805357926eb535c07941dd085b14ec
Norbert Bautista <norbert.bautista@ibm.com>
2022-03-17 23:15:25 +0800

    npm install react-router-dom WIP

---
+++ src/components/Project/AddProject.js
@@ -0,0 +1,12 @@
+//rcc shorcut to create class base component 
+//it can tap to lifecycle hook and state
+import React, { Component } from 'react'
+
+class AddProject extends Component {
+    render() {
+        return (
+            <div><h1>AddProject</h1></div>
+        )
+    }
+}
+export default AddProject;
---
+++ src/components/Project/CreateProjectButton.js
@@ -0,0 +1,16 @@
+//rfc shortcut to create funtion 
+//function is stateless and working with props
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const CreateProjectButton = () => {
+    return (
+        <React.Fragment>
+            <Link to="/addProject" className="btn btn-lg btn-info">
+                Create a Project
+            </Link>
+        </React.Fragment>
+    )
+}
+export default CreateProjectButton;
