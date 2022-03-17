d9713f7760072aab97dfd9b9989d2fd31dd16b25
Norbert Bautista <norbert.bautista@ibm.com>
2022-03-17 23:27:35 +0800

    updated router link

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Dashboard />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
       </div>
     </Router>
   );
