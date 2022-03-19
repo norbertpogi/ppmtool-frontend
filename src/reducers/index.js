06073b13d69ac89549b8eff5b6be0fcfcfd8536b
Norbert Bautista <norbert.bautista@ibm.com>
2022-03-19 11:03:05 +0800

    created new project via api call with redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 //this is the meeting place of all reducers
 import { combineReducers } from "redux";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
-
-})
+    errors: errorReducer
+});
