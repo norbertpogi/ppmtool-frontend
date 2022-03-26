4f7c0f47c124065a407c61de8bfa66cc3709ab4d
Norbert Bautista <norbert.bautista@ibm.com>
2022-03-26 13:50:57 +0800

    working on projectBoard Backlog

--- src/components/ProjectBoard/ProjectBoard.js
+++ src/components/ProjectBoard/ProjectBoard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import ProjectTask from './ProjectTasks/ProjectTask';
 import { Link } from 'react-router-dom';
+import Backlog from './Backlog';
 
 class ProjectBoard extends Component {
 
@@ -13,41 +13,7 @@ class ProjectBoard extends Component {
                 </Link>
                 <br />
                 <hr />
-                {/*-- Backlog STARTS HERE -->*/}
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-secondary text-white">
-                                    <h3>TO DO</h3>
-                                </div>
-                            </div>
-                            <ProjectTask />
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-primary text-white">
-                                    <h3>In Progress</h3>
-                                </div>
-                            </div>
-                            { /*SAMPLE PROJECT TASK STARTS HERE */}
-
-                            {/*-- SAMPLE PROJECT TASK ENDS HERE -->*/}
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-success text-white">
-                                    <h3>Done</h3>
-                                </div>
-                            </div>
-                            {/*-- SAMPLE PROJECT TASK STARTS HERE --*/}
-
-                            {/*-- SAMPLE PROJECT TASK ENDS HERE -->*/}
-                        </div>
-                    </div>
-                </div>
-
-                {/*-- Backlog ENDS HERE --*/}
+                <Backlog />
             </div>
         )
     }
