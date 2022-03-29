bd6aa44bb960f849ad01efb521e62c70b84378ae
norbert <norbert.bautista.99@gmail.com>
2022-03-29 13:40:47 +0800

    added backlog projectBoard validation

--- src/actions/backlogActions.js
+++ src/actions/backlogActions.js
@@ -1,30 +1,38 @@
 import axios from "axios";
-import { DELETE_PROJECT_TASK, GET_BACKLOG, GET_ERRORS, GET_PROJECT_TASK } from "./types";
+import {
+  DELETE_PROJECT_TASK,
+  GET_BACKLOG,
+  GET_ERRORS,
+  GET_PROJECT_TASK,
+} from "./types";
 
-export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
+export const addProjectTask =
+  (backlog_id, project_task, history) => async (dispatch) => {
     try {
-        await axios.post(`/backlog/${backlog_id}`, project_task);
-        history.push(`/projectBoard/${backlog_id}`);
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
+      await axios.post(`/backlog/${backlog_id}`, project_task);
+      history.push(`/projectBoard/${backlog_id}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {},
+      });
     } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data,
+      });
     }
-}
-export const getBackLog = (backlog_id, history) => async dispatch => {
-    try {
-        const resData = await axios.get(`/backlog/${backlog_id}`);
-        dispatch({
-            type: GET_BACKLOG,
-            payload: resData.data
-        });
-    } catch (error) {
-        history.push(`/projectBoard/${backlog_id}`);
-    }
-
-}
+  };
+export const getBackLog = (backlog_id) => async (dispatch) => {
+  try {
+    const resData = await axios.get(`/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: resData.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
--- src/components/ProjectBoard/ProjectBoard.js
+++ src/components/ProjectBoard/ProjectBoard.js
@@ -1,39 +1,67 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import Backlog from './Backlog';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Backlog from "./Backlog";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { getBackLog } from '../../actions/backlogActions';
+import { getBackLog } from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
-    //constructor to handle errors
+  //constructor to handle errors
+  constructor() {
+    super();
+    this.state = {
+      errors: {},
+    };
+  }
 
-    componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.getBackLog(id, this.props.history);
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
+  }
 
-    render() {
-        const { id } = this.props.match.params;
-        const {project_tasks } = this.props.backlog;
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBackLog(id);
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+    const { project_tasks } = this.props.backlog;
+    const { errors } = this.state;
+    let boardContent;
+    const boardAlgorithm = (errors, project_tasks) => {
+      if (project_tasks.length < 1 || errors.projectIdentifier) {
         return (
-            <div className="container">
-                <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
-                    <i className="fas fa-plus-circle"> Create Project Task</i>
-                </Link>
-                <br />
-                <hr />
-                <Backlog project_task_prop={project_tasks }/>
-            </div>
-        )
-    }
+          <div className="alert alert-danger text-center" role="alert">
+            No Project Tasks on this board
+          </div>
+        );
+      }
+      return <Backlog project_task_prop={project_tasks} />;
+    };
+    boardContent = boardAlgorithm(errors, project_tasks);
+    return (
+      <div className="container">
+        <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
+          <i className="fas fa-plus-circle"> Create Project Task</i>
+        </Link>
+        <br />
+        <hr />
+
+        {boardContent}
+      </div>
+    );
+  }
 }
 ProjectBoard.propTypes = {
-    backlog: PropTypes.object.isRequired,
-    getBacklog: PropTypes.func.isRequired
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    backlog: state.backlog
+const mapStateToProps = (state) => ({
+  backlog: state.backlog,
+  errors: state.errors,
 });
 export default connect(mapStateToProps, { getBackLog })(ProjectBoard);
