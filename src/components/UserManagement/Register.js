98855504e07d51190664ab514b83bc9f881e76ed
norbert <norbert.bautista.99@gmail.com>
2022-04-12 10:17:10 +0800

    added user registration and validation via api call

--- src/components/UserManagement/Register.js
+++ src/components/UserManagement/Register.js
@@ -1,37 +1,108 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createNewUser } from "../../actions/securityActions";
+import classNames from 'classnames';
 
 class Register extends Component {
-  render() {
-    return (
-        <div className="register">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 m-auto">
-                    <h1 className="display-4 text-center">Sign Up</h1>
-                    <p className="lead text-center">Create your Account</p>
-                    <form action="create-profile.html">
-                        <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" placeholder="Name" name="name"
-                                required />
-                        </div>
-                        <div className="form-group">
-                            <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+  constructor() {
+    super();
 
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" placeholder="Confirm Password"
-                                name="password2" />
-                        </div>
-                        <input type="submit" className="btn btn-info btn-block mt-4" />
-                    </form>
+    this.state = {
+      username: "",
+      fullName: "",
+      password: "",
+      confirmPassword: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+        username: this.state.username,
+        fullName: this.state.fullName,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword
+    }
+    console.log(newUser);
+    this.props.createNewUser(newUser, this.props.history);
+}
+componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+        this.setState({ errors: nextProps.errors })
+    }
+
+}
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your Account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={classNames("form-control form-control-lg", { "is-invalid": errors.fullName })}
+                    placeholder="Full name"
+                    name="fullName"
+                    value={this.state.fullName}                    
+                    onChange={this.onChange}  />
+                    {errors.fullName && (<div className='invalid-feedback'>{errors.fullName}</div>)}
                 </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={classNames("form-control form-control-lg", { "is-invalid": errors.username })}
+                    placeholder="Email Address"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange}  />
+                    {errors.username && (<div className='invalid-feedback'>{errors.username}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classNames("form-control form-control-lg", { "is-invalid": errors.password })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange} />
+                    {errors.password && (<div className='invalid-feedback'>{errors.password}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classNames("form-control form-control-lg", { "is-invalid": errors.confirmPassword })}
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    value={this.state.confirmPassword}
+                    onChange={this.onChange} />
+                    {errors.confirmPassword && (<div className='invalid-feedback'>{errors.confirmPassword}</div>)}
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
+          </div>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
-export default Register
+Register.propTypes = {
+    createNewUser: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+}
+const mapToStateProps = state => ({
+    errors: state.errors
+});
+export default connect(mapToStateProps,{createNewUser})(Register);
