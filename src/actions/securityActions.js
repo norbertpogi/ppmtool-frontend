bbf3c7135c1e7fd9b46d8b6129f14e45f82c4bcf
norbert <norbert.bautista.99@gmail.com>
2022-04-14 12:40:29 +0800

    work with usersLogin security(securityReducer and securityAction)

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,18 +1,40 @@
 import axios from "axios";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
-export const createNewUser= (newUser, history) => async (dispatch) => {
-    try {
-        await axios.post(`/users/register`, newUser);
-        history.push(`/login`);
-        dispatch({
-          type: GET_ERRORS,
-          payload: {},
-        });
-      } catch (error) {
-        dispatch({
-          type: GET_ERRORS,
-          payload: error.response.data,
-        });
-      }
-    };
+export const createNewUser = (newUser, history) => async (dispatch) => {
+  try {
+    await axios.post(`/users/register`, newUser);
+    history.push(`/login`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    const res = await axios.post("/users/login", LoginRequest); // post => Login Request
+    const { token } = res.data; // extract token from res.data
+    localStorage.setItem("jwtToken", token); // store the token in the localStorage
+    setJWTToken(token); // set our token in header ***
+    const decoded = jwt_decode(token); // decode token on React
+    
+    dispatch({ // dispatch to our securityReducer
+      type: SET_CURRENT_USER,
+      payload: decoded,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import backLogReducer from "./backLogReducer";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import securityReducer from "./securityReducer";
 
 export default combineReducers({
     errors: errorReducer,
     project: projectReducer,
-    backlog: backLogReducer
+    backlog: backLogReducer,
+    security: securityReducer
 });
